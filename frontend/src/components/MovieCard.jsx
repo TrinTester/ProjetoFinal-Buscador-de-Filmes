ea27d68345fecb8c45599e49c01924e5110f11c6
TrinTester <abner.trindade@sempreceub.com>
2025-05-17 22:28:31 -0300

    Update MovieCard.jsx

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default function MovieCard({ movie }) {
   const imgUrl = movie.poster_path
     ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
@@ -12,18 +11,3 @@ export default function MovieCard({ movie }) {
     </div>
   );
 }
-=======
-export default function MovieCard({ movie }) {
-  const imgUrl = movie.poster_path
-    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-    : "https://via.placeholder.com/200x300?text=Sem+Imagem";
-
-  return (
-    <div className="movie-card">
-      <img src={imgUrl} alt={movie.title} className="poster" />
-      <h2>{movie.title}</h2>
-      <p>{movie.release_date}</p>
-    </div>
-  );
-}
->>>>>>> master
